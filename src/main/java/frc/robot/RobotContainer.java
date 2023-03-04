a12fdb0ae1ad9f34ee1934a7d3f5e6cd1f2d9728
CodingTimeYog <yogeshchess77@gmail.com>
2023-03-04 10:56:01 -0500

    Added Shuffleboard for 3 positions w/ commands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,18 +4,25 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.DrivetrainSubsystem;
 
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.commands.AutonomousDistance;
 import frc.robot.commands.DefaultDriveCommand;
+import frc.robot.commands.DrivePositionCommand;
 import frc.robot.commands.PlatformDockPidCommand_Pitch;
 
 
@@ -27,8 +34,19 @@ public class RobotContainer {
     final CommandXboxController m_controller = new CommandXboxController(0);
     final CommandXboxController m_subcontroller = new CommandXboxController(1);
     Command zeroJoystickCommand;
-    
-    
+    final ShuffleboardTab tab =  Shuffleboard.getTab("Main Tab");
+    // Change these to match our plan for each position
+    private final Command m_leftPositionCommand =
+    new AutonomousDistance(
+        m_drivetrainSubsystem);
+private final Command m_middlePositionCommand = getAutonomousCommand();
+private final Command m_rightPositionCommand = 
+new DrivePositionCommand(m_drivetrainSubsystem, 
+new Pose2d(5.0, 4.9, m_drivetrainSubsystem.getGyroscopeRotation()));
+
+// A chooser for autonomous commands
+SendableChooser<Command> m_chooser = new SendableChooser<>();
+
     
     public RobotContainer() {
         // Set up the default command for the drivetrain.
@@ -36,8 +54,10 @@ public class RobotContainer {
         // Left stick Y axis -> forward and backwards movement
         // Left stick X axis -> left and right movement
         // Right stick X axis -> rotation
-        
-        
+        m_chooser.setDefaultOption("Right Position", m_rightPositionCommand);
+        m_chooser.addOption("Left Position", m_leftPositionCommand);
+        m_chooser.addOption("Middle Position", m_middlePositionCommand);
+        tab.add(m_chooser);
         //Prabhu Initialize Drive system to forward facing
         m_drivetrainSubsystem.drive(
         ChassisSpeeds.fromFieldRelativeSpeeds(
@@ -84,7 +104,8 @@ public class RobotContainer {
             public Command getAutonomousCommand( ){
                 // An ExampleCommand will run in autonomous
                 //return new InstantCommand();
-                return new PlatformDockPidCommand_Pitch(m_drivetrainSubsystem);
+                // This can be changed to another command group type like parallel 
+                return new SequentialCommandGroup(new PlatformDockPidCommand_Pitch(m_drivetrainSubsystem), m_chooser.getSelected());
             }
             
             private static double deadband(double value, double deadband) {
